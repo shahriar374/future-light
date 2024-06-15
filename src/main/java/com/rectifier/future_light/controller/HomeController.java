3a8d3ef039630fb9fbb0581504724d2bc0be5702
shahriar374 <68866658+shahriar374@users.noreply.github.com>
2024-06-15 12:16:33 +0600

    Connected database and added a sample home page.

---
+++ src/main/java/com/rectifier/future_light/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.rectifier.future_light.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String home() {
+		return "home";
+	}
+
+}
