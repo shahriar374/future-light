a6f9f30a277c4cbe1192db3559e7535297fdff23
shahriar374 <68866658+shahriar374@users.noreply.github.com>
2024-06-15 09:39:21 +0600

    Initial Commit

---
+++ src/main/java/com/rectifier/future_light/FutureLightApplication.java
@@ -0,0 +1,13 @@
+package com.rectifier.future_light;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureLightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutureLightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rectifier/future_light/FutureLightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rectifier.future_light;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutureLightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
