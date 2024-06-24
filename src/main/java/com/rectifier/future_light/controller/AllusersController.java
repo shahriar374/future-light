a3d7f1aa78881ed2358dc0b9a259000f3c1cd6cf
shahariarkabir374 <shahariarkabir374@gmail.com>
2024-06-24 07:51:11 +0000

    Edit infromation implemented on all users page

---
+++ src/main/java/com/rectifier/future_light/controller/AllusersController.java
@@ -0,0 +1,23 @@
+package com.rectifier.future_light.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.rectifier.future_light.dto.SignupDto;
+import com.rectifier.future_light.service.DashboardService;
+
+@Controller
+public class AllusersController {
+
+    @Autowired
+    DashboardService dashboardService;
+    
+    @PostMapping("/allusers/update")
+	public String updateDashboardInfo(SignupDto dto) {
+		dashboardService.updateUser(dto);
+
+		return "redirect:/allusers?updated";
+	}
+
+}
