bb934289e758d3b59a8fff3c17db2b759d3887be
shahriar374 <shahariarkabir374@gmail.com>
2024-09-25 02:47:43 +0600

    Gene reconstruction function connected to Machine learning model

--- src/main/java/com/rectifier/future_light/service/GeneService.java
+++ src/main/java/com/rectifier/future_light/service/GeneService.java
@@ -1,19 +1,28 @@
 package com.rectifier.future_light.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
 
 @Service
 public class GeneService {
-    
-    public String reconstructDna(String userDnaSequence, String partnerDnaSequence) {
 
-        // Not real algorithm
-        for (int i = 0; i < 3; i++) {
-            userDnaSequence += partnerDnaSequence; 
-            partnerDnaSequence += userDnaSequence;
-        }
+    private final WebClient webClient = WebClient.builder().baseUrl("http://localhost:5000").build();
 
-        return userDnaSequence + partnerDnaSequence;
+    public String reconstructDna(String sequence1, String sequence2) {
+        return webClient.post()
+                .uri("/reconstruction")
+                .bodyValue(Map.of("sequence1", sequence1, "sequence2", sequence2))
+                .retrieve()
+                .bodyToMono(Map.class)
+                .map(response -> (String) response.get("predicted_sequence"))
+                .block();
+    }
+
+    public static boolean isValidDNASequence(String dnaSequence) {
+        // Check if the sequence is not null and contains only 'A', 'C', 'G', or 'T'
+        return dnaSequence != null && dnaSequence.matches("[ACGT]+");
     }
 
 }
