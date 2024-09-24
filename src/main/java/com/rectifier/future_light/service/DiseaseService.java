bf3f35bfb601ba504b4ea82ad791b89ba1a19268
shahriar374 <shahariarkabir374@gmail.com>
2024-09-25 02:34:24 +0600

    Disease detection function connected to Machine learning model

--- src/main/java/com/rectifier/future_light/service/DiseaseService.java
+++ src/main/java/com/rectifier/future_light/service/DiseaseService.java
@@ -3,23 +3,27 @@ package com.rectifier.future_light.service;
 import java.util.Map;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class DiseaseService {
 
-    public Map<String, Double> predictDisease(String dnaSequence, int age) {
+    private final WebClient webClient = WebClient.builder().baseUrl("http://localhost:5000").build();
 
-        // Not real algorithm
-        Map<String, Double> diseases = Map.of(
-                "heartAttack", Math.round((Math.random() * 100) * 100) / 100.0,
-                "kidneyFailure", Math.round((Math.random() * 100) * 100) / 100.0,
-                "cancer", Math.round((Math.random() * 100) * 100) / 100.0,
-                "diabetes", Math.round((Math.random() * 100) * 100) / 100.0,
-                "stroke", Math.round((Math.random() * 100) * 100) / 100.0,
-                "arthritis", Math.round((Math.random() * 100) * 100) / 100.0,
-                "alzheimers", Math.round((Math.random() * 100) * 100) / 100.0);
+    public String predictDisease(String dnaSequence) {
+        return webClient.post()
+                .uri("/predict")
+                .bodyValue(Map.of("dna_sequence", dnaSequence))
+                .retrieve()
+                .bodyToMono(Map.class)
+                .map(response -> (String) response.get("predicted_disease"))
+                .block();
+    }
 
-        return diseases;
+    public static boolean isValidDNASequence(String dnaSequence) {
+        // Check if the sequence is not null and contains only 'A', 'C', 'G', or 'T'
+        return dnaSequence != null && dnaSequence.matches("[ACGT]+");
     }
 
 }
